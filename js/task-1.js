5f7e8ccc8e59aa0c6b6ef42a834e22875cca98a9
SaNya <kushpita.o@gmail.com>
2025-03-24 18:23:13 +0200

    end homework 8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const categoriesList = document.querySelectorAll('#categories .item');
-console.log(`Number of categories: ${categoriesList.length}`);
-
-categoriesList.forEach(category => {
-  console.log(`Category: ${category.querySelector('h2').textContent}`);
-  console.log(`Elements: ${category.querySelectorAll('ul li').length}`);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-input.addEventListener('input', onInputChange);
-
-function onInputChange() {
-  output.textContent = input.value.trim() ? input.value : 'Anonymous';
-}
